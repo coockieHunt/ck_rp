5454f73b6ce745e9937d5ba4f13b8d28454990c2
unknown <jonathan.gleyze@gmail.com>
2020-01-04 21:55:26 +0100

    fix spawn survival ui

--- hub/client/cl_hub.lua
+++ hub/client/cl_hub.lua
@@ -10,7 +10,21 @@ AddEvent("OnPlayerSpawn", function(playerid)
     SetWebVisibility(survival_ui, WEB_VISIBLE)
 end)
 
-AddRemoteEvent("updateHud_survival", function(health, armor)
+function OnWebLoadComplete(webid)
+	if (survival_ui == webid) then
+        Delay(500, function(webid)
+            local health = math.floor(GetPlayerHealth())
+            local armor = math.floor(GetPlayerArmor())
+            ExecuteWebJS(survival_ui, "SetHealth('"..health.."');")
+            ExecuteWebJS(survival_ui, "SetArmor('"..armor.."');")
+		end, survival_ui)
+	end
+end
+AddEvent("OnWebLoadComplete", OnWebLoadComplete)
+
+AddRemoteEvent("updateHud_survival", function()
+    local health = math.floor(GetPlayerHealth())
+    local armor = math.floor(GetPlayerArmor())
     ExecuteWebJS(survival_ui, "SetHealth('"..health.."');")
     ExecuteWebJS(survival_ui, "SetArmor('"..armor.."');")
 end)
--- hub/server/sr_hub.lua
+++ hub/server/sr_hub.lua
@@ -1,7 +1,4 @@
 function OnPlayerDamage(playerid)
-   local health = math.floor(GetPlayerHealth(playerid))
-   local armor = math.floor(GetPlayerArmor(playerid))
-
-   CallRemoteEvent(playerid, "updateHud_survival", health, armor)
+   CallRemoteEvent(playerid, "updateHud_survival")
 end
 AddEvent("OnPlayerDamage", OnPlayerDamage)
