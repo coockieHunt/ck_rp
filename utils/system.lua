d22e197cd24d4b0da8385cf96d09c923f4d11a82
unknown <jonathan.gleyze@gmail.com>
2020-01-07 05:35:52 +0100

    add switch function

--- utils/system.lua
+++ utils/system.lua
@@ -3,3 +3,17 @@ function GetSystemTime()
     local time = os.date('%Y-%m-%d %H:%M:%S', ts)
     return time
 end
+
+function switch(t)
+    t.case = function (self,x)
+      local f=self[x] or self.default
+      if f then
+        if type(f)=="function" then
+          f(x,self)
+        else
+          return f
+        end
+      end
+    end
+    return t
+  end
