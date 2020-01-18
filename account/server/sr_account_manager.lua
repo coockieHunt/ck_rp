d6f626bd240aebc1d21f2b895bc65ebe5e223449
Jonathan <jonathan.gleyze@gmail.com>
2020-01-18 15:08:54 +0100

    add fille for clarity

---
+++ account/server/sr_account_manager.lua
@@ -0,0 +1,30 @@
+---- get player info
+function getplayer(player)
+    local steam_id = tostring(GetPlayerSteamId(player))
+    for key, valeu in pairs(playerData) do
+        if(valeu.steamId == steam_id) then
+            return valeu
+        end
+    end
+end
+
+---- sav
+function SaveAccountPlayer(player)
+	local steam_id = tostring(GetPlayerSteamId(player))
+    local Data = {}
+    for key, value in pairs(playerData) do
+        if(value.steamId == steam_id) then
+            Data = value
+        end
+    end
+
+    local query = mariadb_prepare(db, Request_account:case("SaveAccount"),
+        Data.cash,
+        Data.cash_account,
+        Data.health,
+        Data.armor,
+		Data.steamId
+	)
+        
+	mariadb_query(db, query)
+end
