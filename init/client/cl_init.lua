320d197e765f7ec3451f223a02995e0bebc38908
Jonathan <jonathan.gleyze@gmail.com>
2020-01-18 18:48:02 +0100

    add first person view key "v"

--- init/client/cl_init.lua
+++ init/client/cl_init.lua
@@ -3,3 +3,19 @@ function spawn_init_clothing(player, random_id)
 	SetPlayerClothingPreset(GetPlayerId(),id)
 end
 AddRemoteEvent("spawn_init_clothing", spawn_init_clothing)
+
+function OnKeyPress(key)
+    if key == "V" then
+		if IsFirstPersonCamera() then
+			EnableFirstPersonCamera(false)
+		else
+			EnableFirstPersonCamera(true)
+			SetNearClipPlane(10)
+		end
+	end
+end
+AddEvent("OnKeyPress", OnKeyPress)
+
+AddEvent("OnPlayerSpawn", function()
+	SetPostEffect("DepthOfField", "Distance", 0)
+end)
