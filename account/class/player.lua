f83a23b93f833271a09571fe5398818200c969c0
Jonathan <jonathan.gleyze@gmail.com>
2020-01-25 17:33:11 +0100

    clear class

--- account/class/player.lua
+++ account/class/player.lua
@@ -7,38 +7,6 @@ function player:init(ply)
    return ply
 end
 
-function player:getsteamId()
-   return self.money
-end
-
-function player:setsteamId(id)
-   self.money = id
-end
-
-function player:getMoney()
-   return self.money
-end
-
-function player:addMoney(amt)
-   self.money = self.money + amt
-end
-
-function player:removeMoney(amt)
-   self.money = self.money - amt
-end
-
-function player:getBank()
-   return self.bank
-end
-
-function player:removeBank(amt)
-   self.bank = self.bank - amt
-end
-
-function player:addBank(amt)
-   self.bank = self.bank + amt
-end
-
 local function newPlayer(playerInfo)
    return player:init(playerInfo)
 end
