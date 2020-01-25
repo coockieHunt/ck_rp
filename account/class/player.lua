f3736992c10e7de64279615283147b4fcc6911bb
Jonathan <jonathan.gleyze@gmail.com>
2020-01-25 17:58:21 +0100

    add getter setter

--- account/class/player.lua
+++ account/class/player.lua
@@ -7,6 +7,50 @@ function player:init(ply)
    return ply
 end
 
+function player:getsteamId()
+   return self.money
+end
+
+-- HEALTH
+function player:getHealth()
+   return self.health
+end
+
+function player:setHealth(amt)
+   self.health = amt
+   return true
+end
+
+-- ARMOR
+function player:getArmor()
+   return self.armor
+end
+
+function player:setArmor(amt)
+   self.armor = amt
+   return true
+end
+
+-- CASH
+function player:getCash()
+   return self.cash
+end
+
+function player:setCash(amt)
+   self.cash = amt
+   return true
+end
+
+-- CASH ACCOUNT
+function player:getCashAccount()
+   return self.cash_account
+end
+
+function player:setCashAccount(amt)
+   self.cash_account = amt
+   return true
+end
+
 local function newPlayer(playerInfo)
    return player:init(playerInfo)
 end
