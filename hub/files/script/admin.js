0b0d4c5ed6730dcb5e3e56d2af1f003b7a1fa341
Jonathan <jonathan.gleyze@gmail.com>
2020-02-06 15:22:20 +0100

    center default dragable

--- hub/files/script/admin.js
+++ hub/files/script/admin.js
@@ -1,13 +1,11 @@
-jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
-                                                $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
-                                                $(window).scrollLeft()) + "px");
-    return this;
-}
 
 $( function() {
-    $("#window").center();
-    $("#window").draggable({containment: "parent", handle: "#top_bar"});
+    $("#window").draggable({
+        start: function( event, ui ) {
+        },
+        containment: "parent", 
+        handle: "#top_bar"
+    });
 } );
+
+
