9312deb6c4c1f6f087e50cdb8714f53485c9e939
Jonathan <jonathan.gleyze@gmail.com>
2020-02-06 14:21:33 +0100

    miss fil au

---
+++ hub/files/script/admin.js
@@ -0,0 +1,13 @@
+jQuery.fn.center = function () {
+    this.css("position","absolute");
+    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
+                                                $(window).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
+                                                $(window).scrollLeft()) + "px");
+    return this;
+}
+
+$( function() {
+    $("#window").center();
+    $("#window").draggable({containment: "parent", handle: "#top_bar"});
+} );
