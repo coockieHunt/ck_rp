ef88af6ede395e9fcad89c1cee6ff3a6150a2a6c
Jonathan <jonathan.gleyze@gmail.com>
2020-02-10 10:42:59 +0100

    add func car

--- admin/server/hub_admin.lua
+++ admin/server/hub_admin.lua
@@ -1,8 +1,19 @@
 AddRemoteEvent("Exucute", function(playerid, json)
     local data = json_decode(json)
     local func = data['func']
+
+
     if(func == 'car') then
-        admin_car_spawn(playerid, data['id'])
+        if(isnil(data['id'])) then
+            AddPlayerChat(playerid, "none id")
+        else
+            local am = false
+            local b = false
+            if(data['autopilote'] == 'on') then am = true end
+            if(data['boost'] == 'on') then b = true end
+            AddPlayerChat(playerid, data['color'])
+            
+            admin_car_spawn(playerid, data['id'], am, b, data['color'])
+        end
     end
-           
 end)
