fed86e968fb69929a4870b0f98e4d90b3d80eff7
Jonathan <jonathan.gleyze@gmail.com>
2020-02-12 17:07:06 +0100

    add clothing preset list

--- data/client/cl_util.lua
+++ data/client/cl_util.lua
@@ -6,21 +6,21 @@
 --[[
   Vehicles
 ]]
-function GetVehicleName(vehicleID)
-  if VEHICLE_DATA[vehicleID] == nil then return false end
-  return VEHICLE_DATA[vehicleID]['name']
+function GetVehicleName(id)
+  if VEHICLE_DATA[id] == nil then return false end
+  return VEHICLE_DATA[id]['name']
 end
 
-function GetVehicleByName(vehicleName)
+function GetVehicleByName(name)
   for i, v in ipairs(VEHICLE_DATA) do
     -- Name
-    if string.lower(v['name']) == string.lower(vehicleName) then
+    if string.lower(v['name']) == string.lower(name) then
       return i
     end
 
     -- Alias
     for _, va in ipairs(VEHICLE_DATA[i]['alias']) do
-      if string.lower(va) == string.lower(vehicleName) then
+      if string.lower(va) == string.lower(name) then
         return i
       end
     end
@@ -41,31 +41,62 @@ function GetAllVehiclesList()
   end
 end
 
-function tablelength(T)
-  local count = 0
-  for _ in ipairs(T) do count = count + 1 end
-  return count
+--[[
+  Clothing preset
+]]
+function GetGlotingPresetName(id)
+  if CLOTHING_PRESET_DATA[id] == nil then return false end
+  return CLOTHING_PRESET_DATA[id]['name']
 end
 
+function GetGlotingPresetByName(name)
+  for i, v in ipairs(CLOTHING_PRESET_DATA) do
+    -- Name
+    if string.lower(v['name']) == string.lower(name) then
+      return i
+    end
+
+    -- Alias
+    for _, va in ipairs(CLOTHING_PRESET_DATA[i]['alias']) do
+      if string.lower(va) == string.lower(name) then
+        return i
+      end
+    end
+  end
+
+  return 0
+end
+
+function GetCountGlotingPresetList()
+  return tablelength(CLOTHING_PRESET_DATA)
+end
+
+function GetAllGlotingPresetList()
+  for i, v in ipairs(CLOTHING_PRESET_DATA) do
+    local name = v['name']
+    local alias = CLOTHING_PRESET_DATA[i]['alias'][1]
+    local id = i
+  end
+end
 
 --[[
   Weapons
 ]]
-function GetWeaponName(weaponID)
-  if WEAPON_DATA[weaponID] == nil then return false end
-  return WEAPON_DATA[weaponID]['name']
+function GetWeaponName(id)
+  if WEAPON_DATA[id] == nil then return false end
+  return WEAPON_DATA[id]['name']
 end
 
-function GetWeaponByName(weaponName)
+function GetWeaponByName(name)
   for i, v in ipairs(WEAPON_DATA) do
     -- Name
-    if string.lower(v['name']) == string.lower(weaponName) then
+    if string.lower(v['name']) == string.lower(name) then
       return i
     end
 
     -- Alias
     for _, va in ipairs(WEAPON_DATA[i]['alias']) do
-      if string.lower(va) == string.lower(weaponName) then
+      if string.lower(va) == string.lower(name) then
         return i
       end
     end
