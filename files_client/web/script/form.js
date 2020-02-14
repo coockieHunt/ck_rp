cf55936749d117bd62f290b2f0f1cdd01882f0f3
Jonathan <jonathan.gleyze@gmail.com>
2020-02-14 16:18:14 +0100

    add arrow dp menu

--- files_client/web/script/form.js
+++ files_client/web/script/form.js
@@ -3,6 +3,7 @@ $( function() {
     //hide dp
     $( ".dp" ).each(function( i ) {
         let dp_list = $(this).attr('id')
+        $(this).append( "<img src='../../files_client/web/src/img/arrow_down.png' height='10' width='10' class='dp_icon'>" );
         $("#"+dp_list+"_dp").slideUp(200)
         $("#"+dp_list+"_dp").data('state', "up")
     });
@@ -10,12 +11,17 @@ $( function() {
     $( ".dp" ).click(function() {
         let dp_list = $(this).attr('id')
         let state = $(this).data('state')
+        let img = $( this ).children()
+
         if(state == "up"){
             $(this).data('state', "down")
             $("#"+dp_list+"_dp").slideDown(200)
+            img.attr('src', '../../files_client/web/src/img/arrow_up.png');
+            console.log(img)
         }else{
             $(this).data('state', "up")
             $("#"+dp_list+"_dp").slideUp(200)
+            img.attr('src', '../../files_client/web/src/img/arrow_down.png');
         }
     });
 
