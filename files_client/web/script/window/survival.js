439e691dbe26b8a7c84e214245bff7e5dae73dd3
Jonathan <jonathan.gleyze@gmail.com>
2020-02-21 11:04:48 +0100

    update survival ui

--- files_client/web/script/window/survival.js
+++ files_client/web/script/window/survival.js
@@ -1,27 +1,27 @@
 function SetHealth(new_health)
 {
-    let healt = document.querySelector("#healt > .progress > .progress-container > .progressbar-element");
+    let healt = document.querySelector("#stats > #health > .progress > .progress-container > .progressbar-element");
     healt.style.width = new_health + "%";
     return true
 }
 
 function SetArmor(new_armor)
 {
-    let armor = document.querySelector(" #armor > .progress > .progress-container > .progressbar-element");
+    let armor = document.querySelector("#stats > #armor > .progress > .progress-container > .progressbar-element");
     armor.style.width = new_armor + "%";
     return true
 }
 
 function SetCash(new_cash)
 {
-    let cash = document.querySelector("#cash > .cash_data" );
+    let cash = document.querySelector("#stats > #cash > #data");
     cash.innerHTML = new_cash
     return true
 }
 
 function SetBank(net_account_cash)
 {
-    let cash_account = document.querySelector("#bank > .cash_account_data" );
+    let cash_account = document.querySelector("#stats > #bank > #data");
     cash_account.innerHTML = net_account_cash
     return true
 }
