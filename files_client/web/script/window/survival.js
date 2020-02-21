e4d34104f0dde5eff427ce37a37024d88b7a900f
Jonathan <jonathan.gleyze@gmail.com>
2020-02-21 11:44:36 +0100

    fix header title

--- files_client/web/script/window/survival.js
+++ files_client/web/script/window/survival.js
@@ -1,27 +1,27 @@
 function SetHealth(new_health)
 {
-    let healt = document.querySelector("#stats > #health > .progress > .progress-container > .progressbar-element");
+    let healt = document.querySelector("#stats > .content > #health > .progress > .progress-container > .progressbar-element");
     healt.style.width = new_health + "%";
     return true
 }
 
 function SetArmor(new_armor)
 {
-    let armor = document.querySelector("#stats > #armor > .progress > .progress-container > .progressbar-element");
+    let armor = document.querySelector("#stats > .content > #armor > .progress > .progress-container > .progressbar-element");
     armor.style.width = new_armor + "%";
     return true
 }
 
 function SetCash(new_cash)
 {
-    let cash = document.querySelector("#stats > #cash > #data");
+    let cash = document.querySelector("#stats > .content > #cash > #data");
     cash.innerHTML = new_cash
     return true
 }
 
 function SetBank(net_account_cash)
 {
-    let cash_account = document.querySelector("#stats > #bank > #data");
+    let cash_account = document.querySelector("#stats > .content > #bank > #data");
     cash_account.innerHTML = net_account_cash
     return true
 }
