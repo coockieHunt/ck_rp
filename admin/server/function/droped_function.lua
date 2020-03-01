60b60ccf663c283e12fb83f5755f81715d8fab8b
Jonathan <jonathan.gleyze@gmail.com>
2020-03-01 11:43:45 +0100

    add admin log

--- admin/server/function/droped_function.lua
+++ admin/server/function/droped_function.lua
@@ -2,6 +2,7 @@ function admin_delete_droped_Item(player, droped_id)
 	local level = 1
 
     if(AdminLevel(player, level)) then
+		AddAdminLog(player, "Droped item id : " .. droped_id.. " has been deleted")
         RemoveDropedItem(droped_id)
 	end
 end
@@ -10,6 +11,7 @@ function admin_teleport_droped_Item(player, droped_id)
 	local level = 1
 
     if(AdminLevel(player, level)) then
+		AddAdminLog(player, "Teleported item id : " .. droped_id)
         local object = GetDropedItemById(tonumber(droped_id))
         local object_pos = object['pos']
         admin_tp_to_pos(player, object_pos[1], object_pos[2], object_pos[3])
