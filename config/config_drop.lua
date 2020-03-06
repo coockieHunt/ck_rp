74d8602db8d182902624456ef43450a6f820a7ad
Jonathan <jonathan.gleyze@gmail.com>
2020-03-06 16:26:52 +0100

    fix animation time

--- config/config_drop.lua
+++ config/config_drop.lua
@@ -1,11 +1,11 @@
 -- drop animation
 _Drop_animation = {
     animation_id = "PICKUP_LOWER",
-    detach_time = 1.4
+    detach_time = 1400
 };
 
 _Gather_animation = {
     animation_id = "PICKUP_LOWER",
-    atach_time = 1.4,
-    detach_time = 1.0
+    atach_time = 1400,
+    detach_time = 1400
 }
