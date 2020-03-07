2f02454bee6519b05edc0631c127ae1b843d5fa1
Jonathan <jonathan.gleyze@gmail.com>
2020-03-07 20:32:07 +0100

    refacotre config file

--- config/config_system.lua
+++ config/config_system.lua
@@ -1,11 +1,10 @@
--- config server package autoload
+-- Package
 _Pacakge = {
     server_folder = "server",
     display_console = true,
     log_file = "log_pacakge.txt",
 };
 
--- list package load
 _Import_package = {
     "log",
     "inventory",
@@ -18,4 +17,46 @@ _Import_package = {
     "admin",
     "utils",
     "cop"
+};
+
+-- DROP
+_Drop_animation = {
+    animation_id = "PICKUP_LOWER",
+    detach_time = 1400
+};
+
+_Gather_animation = {
+    animation_id = "PICKUP_LOWER",
+    atach_time = 1400,
+    detach_time = 1400
+}
+
+-- LOG
+_Log_color = {
+    ['error'] = "F82C00",
+    ['warning'] = "E5B100",
+    ['info'] = "0438CE",
+    ['ok'] = "10CE04"
+  };
+  
+_Log_file = {
+    folder = "logs",
+    admin = "log_admin.txt",
+    login = "log_login.txt",
+    commande = "log_commande.txt"
+};
+    
+-- SQL
+_Mariadb = { 
+    host = "127.0.0.1", 
+    user = "root", 
+    password = "root", 
+    database = "onset"
+  };
+  
+_RequestSql = {
+    IfplayerAccountExist = "SELECT id FROM accounts WHERE steam_id = '?' LIMIT 1;",
+    CreatePlayerAccount = "INSERT INTO accounts (id, steam_id, health, armor, weight, player_name, cash, cash_account, inventory) VALUES (NULL, '?', '?', '?', '?', '?', '?', '?', '?');",
+    GetPlayerAccount = "SELECT accounts.id, steam_id, health, armor, player_name, cash, cash_account, inventory, max_weight, account_administrator.admin_level FROM account_administrator RIGHT JOIN accounts ON account_administrator.account_id = accounts.id WHERE steam_id = '?';",
+    SaveAccount = "UPDATE accounts SET cash= ?, cash_account=?, health= ?, armor=?, inventory='?'  WHERE steam_id = ? LIMIT 1;"
 };
