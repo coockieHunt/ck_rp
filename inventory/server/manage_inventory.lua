ca36e31808ff7691485bcdba031bd5db4d9bf911
Jonathan <jonathan.gleyze@gmail.com>
2020-03-11 17:01:17 +0100

    wieght manage add item

--- inventory/server/manage_inventory.lua
+++ inventory/server/manage_inventory.lua
@@ -2,6 +2,12 @@ function AddPlayerItem(player, target, item_id, quantity)
     if(CheckIfValidItem(item_id) or isnil(target) or isnil(quantity)) then
         local decode_inventory = GetInventory(target)
 
+        local new_currentWeight = math.floor(CalculateInvWeight(target) + GetInventoryItemWeight(item_id, quantity))
+
+        if(GetInventoryMaxWeight(target) < new_currentWeight) then
+            return false
+        end
+
         local cur_quantity = GetItemsQuantity(decode_inventory, item_id)
 
         if(cur_quantity ~= false) then
@@ -14,7 +20,9 @@ function AddPlayerItem(player, target, item_id, quantity)
 
         table.insert(decode_inventory, new_item) 
 
-        SaveInventory(target, decode_inventory)
+        SaveInventory(target, decode_inventory, new_currentWeight)
+
+        return true
     else
         print('invalid item or params')
     end
@@ -24,19 +32,15 @@ function RemovePlayerItem(player, target, item_id, quantity)
     if(CheckIfValidItem(item_id) or isnil(target) or isnil(quantity)) then
         local decode_inventory = GetInventory(target)
         local cur_quantity = GetItemsQuantity(decode_inventory, item_id)
+        local new_currentWeight = math.floor(CalculateInvWeight(target) - GetInventoryItemWeight(item_id, quantity))
 
-        if(cur_quantity ~= false) then
+        if(cur_quantity ~= false and new_currentWeight > 0) then
             if(tonumber(quantity) >= 0) then
                 quantity =  tonumber(cur_quantity) - tonumber(quantity)
             end
-
-            if(quantity < 0) then
-                quantity = 0
-            end
         else
-            quantity = 0
+            return false
         end
-
 
         for i, v in pairs(decode_inventory) do
             local cur_id = v['id']
@@ -51,7 +55,9 @@ function RemovePlayerItem(player, target, item_id, quantity)
             end
         end
 
-        SaveInventory(target, decode_inventory)
+        SaveInventory(target, decode_inventory, new_currentWeight)
+
+        return true
     else
         print('invalid item or params')
     end
