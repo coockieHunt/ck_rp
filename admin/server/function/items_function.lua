8be3cfa08bca6800198c9fbba8a2a44f095e2102
Jonathan <jonathan.gleyze@gmail.com>
2020-03-11 16:59:53 +0100

    add error

--- admin/server/function/items_function.lua
+++ admin/server/function/items_function.lua
@@ -2,8 +2,13 @@ function admin_addItem(player, target, item_id, quantity)
 	local level = 1
 
 	if(AdminLevel(player, level)) then
-		AddAdminLog(player, "add item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
-		AddPlayerItem(player, target, item_id, quantity)
+		local add = AddPlayerItem(player, target, item_id, quantity)
+
+		if(add) then
+			AddAdminLog(player, "add item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+		else
+			AddAdminLog(player, "add FAILED inventory to hight item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+		end
 	end
 end
 
@@ -11,7 +16,12 @@ function admin_removeItem(player, target, item_id, quantity)
 	local level = 1
 
 	if(AdminLevel(player, level)) then
-		AddAdminLog(player, "remove item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
-        RemovePlayerItem(player, target, item_id, quantity)
+		local del = RemovePlayerItem(player, target, item_id, quantity)
+		
+		if(del) then
+			AddAdminLog(player, "remove item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+		else
+			AddAdminLog(player, "remove FAILED inventory to low item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+		end
 	end
 end
