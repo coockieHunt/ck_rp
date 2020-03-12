c85728848ae25094ade3b9327bce0bcb6c016d69
Jonathan <jonathan.gleyze@gmail.com>
2020-03-12 14:36:23 +0100

    fix save sql

--- config/config_system.lua
+++ config/config_system.lua
@@ -59,5 +59,5 @@ _RequestSql = {
     IfplayerAccountExist = "SELECT id FROM accounts WHERE steam_id = '?' LIMIT 1;",
     CreatePlayerAccount = "INSERT INTO accounts (id, steam_id, health, armor, max_weight, cur_weight, player_name, cash, cash_account, inventory) VALUES (NULL, '?', '?', '?', '?', '?', '?', '?', '?', '?');",
     GetPlayerAccount = "SELECT accounts.id, accounts.* , account_administrator.admin_level FROM account_administrator RIGHT JOIN accounts ON account_administrator.account_id = accounts.id WHERE steam_id = '?';",
-    SaveAccount = "UPDATE accounts SET cash= ?, cash_account=?, health= ?, armor=?, inventory='?'  WHERE steam_id = ? LIMIT 1;"
+    SaveAccount = "UPDATE accounts SET cash= ?, cash_account=?, health= ?, armor=?, inventory='?', max_weight='?', cur_weight='?' WHERE steam_id = ? LIMIT 1;"
 };
