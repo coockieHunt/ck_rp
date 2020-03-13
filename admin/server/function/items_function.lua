ae29aa9300e7d4e89b48670135d7a7c8ce977d2f
Jonathan <jonathan.gleyze@gmail.com>
2020-03-13 17:36:13 +0100

    add admin item alert

--- admin/server/function/items_function.lua
+++ admin/server/function/items_function.lua
@@ -6,6 +6,7 @@ function admin_addItem(player, target, item_id, quantity)
 
 		if(add == true) then
 			AddAdminLog(player, "add item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+			SendAlert(target, "warning", "Admin", "An administrator give you " .. quantity .. " " .. item_id)
 		else
 			AddAdminLog(player, "Add item id: " .. item_id.. " to failed players : "..GetPlayerName(target)..", extra item count: " .. add)
 		end
@@ -20,6 +21,7 @@ function admin_removeItem(player, target, item_id, quantity)
 		
 		if(del) then
 			AddAdminLog(player, "remove item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+			SendAlert(target, "error", "Admin", "An administrator remove you " .. quantity .. " " .. item_id)
 		else
 			AddAdminLog(player, "remove FAILED inventory to low item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
 		end
