f2a492e8653c946f123b0cb064a131f3ba663235
Jonathan <jonathan.gleyze@gmail.com>
2020-03-14 15:38:02 +0100

    fix log

--- admin/server/function/items_function.lua
+++ admin/server/function/items_function.lua
@@ -8,7 +8,8 @@ function admin_addItem(player, target, item_id, quantity)
 			AddAdminLog(player, "add item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
 			SendAlert(target, "warning", "Admin", "An administrator give you " .. quantity .. " " .. item_id)
 		else
-			AddAdminLog(player, "Add item id: " .. item_id.. " to failed players : "..GetPlayerName(target)..", extra item count: " .. add)
+			SendAlert(player, "error", "Admin", 
+			GetPlayerName(target).." has full inventory, there are " .. add.. " too many")
 		end
 	end
 end
@@ -23,7 +24,7 @@ function admin_removeItem(player, target, item_id, quantity)
 			AddAdminLog(player, "remove item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
 			SendAlert(target, "error", "Admin", "An administrator remove you " .. quantity .. " " .. item_id)
 		else
-			AddAdminLog(player, "remove FAILED inventory to low item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
+			SendAlert(player, "error", "Admin", "Player ".. GetPlayerName (player) .." does not have enough item ".. item_id .." in his inventory")
 		end
 	end
 end
