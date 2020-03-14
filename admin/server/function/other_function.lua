d45e86ba15fa0270dab791281de87af6403b4b79
Jonathan <jonathan.gleyze@gmail.com>
2020-03-14 22:57:28 +0100

    other admin function

---
+++ admin/server/function/other_function.lua
@@ -0,0 +1,27 @@
+function admin_fc(player, disable)
+	local level = 1
+	local _disable = false
+
+	if(AdminLevel(player, level)) then
+		if disable ~= nil then
+			_disable = true
+		end
+		_disable = not _disable
+
+		SetPlayerSpectate(player, _disable)
+		AddAdminLog(player, GetPlayerName(player).." spectate")
+	end
+end
+
+function admin_pos(player)
+	local level = 1
+
+	if(AdminLevel(player, level)) then
+		AddAdminLog(player, " save pos")
+		local x, y, z = GetPlayerLocation(player)
+		local text = x..", "..y..", "..z
+		file = io.open("pos.txt", "a")
+		file:write(text, "\n")
+		file:close()
+	end
+end
