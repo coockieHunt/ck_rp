ddfc7d56f908f9672024fccc449508c80a36f22d
Jonathan <jonathan.gleyze@gmail.com>
2020-03-14 19:31:08 +0100

    change alert

--- admin/server/function/items_function.lua
+++ admin/server/function/items_function.lua
@@ -6,10 +6,10 @@ function admin_addItem(player, target, item_id, quantity)
 
 		if(add == true) then
 			AddAdminLog(player, "add item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
-			SendAlert(target, "warning", "Admin", "An administrator give you " .. quantity .. " " .. item_id)
+			SendAlert(target, "warning", "Admin", "An administrator give you <strong>" .. quantity .. " " .. item_id.."</strong>")
 		else
 			SendAlert(player, "error", "Admin", 
-			GetPlayerName(target).." has full inventory, there are " .. add.. " too many")
+			GetPlayerName(target).." has full inventory, there are <strong>" .. add.. "</strong> too many")
 		end
 	end
 end
@@ -22,9 +22,9 @@ function admin_removeItem(player, target, item_id, quantity)
 		
 		if(del) then
 			AddAdminLog(player, "remove item "..GetPlayerName(target).." id : " .. item_id.. " quantity :" .. quantity)
-			SendAlert(target, "error", "Admin", "An administrator remove you " .. quantity .. " " .. item_id)
+			SendAlert(target, "error", "Admin", "An administrator remove you <strong>" .. quantity .. " " .. item_id.."</strong>")
 		else
-			SendAlert(player, "error", "Admin", "Player ".. GetPlayerName (player) .." does not have enough item ".. item_id .." in his inventory")
+			SendAlert(player, "error", "Admin", "Player <strong>".. GetPlayerName (player) .."</strong> does not have enough item <strong>".. item_id .."</strong> in his inventory")
 		end
 	end
 end
