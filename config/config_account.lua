684a6e286ec32febfb97bc20566b93f5dcdebba7
Jonathan <jonathan.gleyze@gmail.com>
2020-03-14 22:57:52 +0100

    add survival config

--- config/config_account.lua
+++ config/config_account.lua
@@ -3,6 +3,8 @@ _New_account = {
     cash = 500,
     cash_account = 15000,
     health = 100,
+    food = 100,
+    thirst = 100,
     armor = 50
 };
 
@@ -15,7 +17,13 @@ _Init_player = {
     clothing_preset = 5
 };
 
+_Survival = {
+    decrease_food = 1,
+    decrease_thirst = 1,
+}
+
 -- auto fetch time
-_Save_profile = {
-    save_account_time = 300000 -- milisecond (1 min = 60000 milisecond) default - 300000
+_Account_timer = {
+    save_account_time = 60000*5 ,-- milisecond (1 min = 60000 milisecond) default - 300000
+    decrease_survival_timer = 60000*5 -- milisecond (1 min = 60000 milisecond) default - 300000
 };
