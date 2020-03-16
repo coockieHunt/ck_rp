9b6186211bad70125f7b0262dbde10ec8ca493fb
Jonathan <jonathan.gleyze@gmail.com>
2020-03-16 16:55:44 +0100

    ajust gether distance

--- items/client/cl_gather.lua
+++ items/client/cl_gather.lua
@@ -1,6 +1,6 @@
 AddEvent("OnKeyPress", function(key)
     if key == GetKeyMapServer("interact") then
-        local pickup = GetNearestPickUp(100)
+        local pickup = GetNearestPickUp(125)
         if(pickup == false) then return false end
 
         if(pickup.type == "droped_item") then
