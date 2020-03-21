35fb4ebdd1d0f8b2569f180c9fae46964d856b9c
Jonathan <jonathan.gleyze@gmail.com>
2020-03-21 15:29:30 +0100

    default if nill item type

--- items/server/type/type_function.lua
+++ items/server/type/type_function.lua
@@ -2,7 +2,13 @@ function GetItemTypeInfo(item_id)
     if(CheckIfValidItem(item_id)) then
         for _, item in ipairs(GetItemsList()) do
             if(item.id == item_id) then
-                return _Item_type[item.type]
+                local type = _Item_type[item.type]
+
+                if(isnil(type)) then
+                    type = _Item_type["default"]
+                end
+                
+                return type
             end
         end
     end
