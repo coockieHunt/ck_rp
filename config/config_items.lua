2e157c14c87abc31ffd945adeea7a0578abdb5c0
Jonathan <jonathan.gleyze@gmail.com>
2020-03-23 15:41:51 +0100

    add validate config item

--- config/config_items.lua
+++ config/config_items.lua
@@ -4,15 +4,15 @@ _Items = {
         description = "Découvrez la nouvelle Boisson Energisante de Coca-Cola, Sans Taurine. Spread Your Positive",
         model= 1296,
         weight= 1,
-        usable= false,
         health = 0,
         thirst = 5,
         food = 0,
+        usable= "",
         thumbnail= "../../files_client/web/src/img/energi_drink.png",
         type= "drinks",
         hand_pos = {
-            drop = nil,
-            use = nil
+            drop = false,
+            use = false
         }
     },
   
@@ -24,12 +24,12 @@ _Items = {
         health = 0,
         thirst = 0,
         food = 20,
-        usable= false,
+        usable= "",
         thumbnail= "../../files_client/web/src/img/banana.png",
         type= "food",
         hand_pos = {
             drop = {x = 8, y = -3, z = -4,  rx = -90,  ry = 0, rz = 0 },
-            use = nil
+            use = false
         }
     },
 
@@ -41,12 +41,12 @@ _Items = {
         health = 0,
         thirst = -5,
         food = 6,
-        usable= false,
+        usable= "",
         thumbnail= "../../files_client/web/src/img/chips.png",
         type= "food",
         hand_pos = {
             drop = {x = 8, y = -3, z = -4,  rx = 0.0,  ry = 0, rz = 0},
-            use = nil
+            use = false
         }
     },
 
@@ -58,12 +58,12 @@ _Items = {
         health = 75,
         thirst = 0,
         food = 0,
-        usable= false,
+        usable= "",
         thumbnail= "../../files_client/web/src/img/first_aid_kit.png",
         type= "medic",
         hand_pos = {
             drop = {x = 8, y = -3, z = -4,  rx = 0.0,  ry = 0, rz = 0},
-            use = nil
+            use = false
         }
     }
 }
--- items/server/items/items_build.lua
+++ items/server/items/items_build.lua
@@ -1,8 +1,23 @@
 local ItemsData = {}
 ItemsData.ClassItem = import("items/class/item.lua")
 
+local item_key = {
+    ["name"] = "string",
+    ["description"] = "string",
+    ["model"] = "number",
+    ["weight"] = "number",
+    ["health"] = "number",
+    ["thirst"] = "number",
+    ["food"] = "number",
+    ["usable"] = "string",
+    ["thumbnail"] = "string",
+    ["type"] = "string",
+    ["hand_pos"] = "table"
+}
+
 function OnPackageStart(player)
     for id, item in pairs(_Items) do
+        ValidateItemsConfig(item)
         local i = ItemsData.ClassItem.new(
             {
                 ["id"] = id,
@@ -21,8 +36,39 @@ function OnPackageStart(player)
 
         table.insert(ItemsData, i)       
     end
+
+    print("> Successfully "..tablelength(ItemsData).." items loaded")
 end
 AddEvent("OnPackageStart", OnPackageStart)
+
+function ValidateItemsConfig(item)
+    for k, v in pairs(item_key) do
+        if(item[k] == nil) then
+            ServerExit("Error build item ["..item.name..", "..k.." invalide type nill] (EC 004)")
+        end
+        
+        if(v == 'string') then
+            local type = type(item[k])
+            if(type ~= "string") then
+                ServerExit("Error build item ["..item.name..", "..k.." invalide type like string] (EC 005)")
+            end
+        end
+    
+        if(v == 'number') then
+            local type = type(item[k])
+            if(type ~= "number") then
+                ServerExit("Error build item ["..item.name..", "..k.." invalide type like number] (EC 005)")
+            end
+        end
+    
+        if(v == 'table') then
+            local type = type(item[k])
+            if(type ~= "table") then
+                ServerExit("Error build item ["..item.name..", "..k.." invalide type like table] (EC 005)")
+            end
+        end
+    end
+end
 
 function GetItemsList()
     return ItemsData
