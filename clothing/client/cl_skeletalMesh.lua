7d2f388ecedf3ac0ad3db886b9349346443ad2d3
Jonathan <jonathan.gleyze@gmail.com>
2020-03-30 08:17:48 +0200

    update file structure

--- clothing/client/cl_skeletalMesh.lua
+++ clothing/client/cl_skeletalMesh.lua
@@ -1,27 +1,14 @@
-local skeletalMesh = {}
-
-local MeshElement = {
-    ["body"] = "Body",
-    ['hair'] = "Clothing0",
-    ['shirt'] = "Clothing1",
-    ['accessory'] = "Clothing3",
-    ['pants'] = "Clothing4",
-    ['shoes'] = "Clothing5"
-}
-
 function setSkeletalMesh(type, asset)
     local player = GetPlayerId()
-        
-    local SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, MeshElement[type])
+    local SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, __SkeletalMesh[type])
     SkeletalMeshComponent:SetSkeletalMesh(USkeletalMesh.LoadFromAsset(asset))
     SkeletalMeshComponent:SetFloatParameterOnMaterials("PupilScale", 1.5)
 end
 AddRemoteEvent("setSkeletalMesh", setSkeletalMesh)
 
-
 function removeSkeletalMesh(type)
     local player = GetPlayerId()
-	SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, MeshElement[type])
+	SkeletalMeshComponent = GetPlayerSkeletalMeshComponent(player, __SkeletalMesh[type])
 	SkeletalMeshComponent:SetSkeletalMesh(nil)
 end
 AddRemoteEvent("removeSkeletalMesh", removeSkeletalMesh)
