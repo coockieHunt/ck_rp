2f3883821b46e07306265eb404fa89909c7928fa
Jonathan <jonathan.gleyze@gmail.com>
2020-04-01 09:52:50 +0200

    update db

--- ck_rp.sql
+++ ck_rp.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Hôte :                        127.0.0.1
--- Version du serveur:           10.4.11-MariaDB - mariadb.org binary distribution
+-- Version du serveur:           10.4.10-MariaDB - mariadb.org binary distribution
 -- SE du serveur:                Win64
 -- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
@@ -22,50 +22,17 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `steam_id` varchar(20) DEFAULT NULL,
   `health` smallint(6) DEFAULT 100,
   `armor` smallint(6) DEFAULT 100,
+  `food` smallint(11) DEFAULT NULL,
+  `thirst` smallint(11) DEFAULT NULL,
   `max_weight` int(11) DEFAULT NULL,
   `cur_weight` int(11) DEFAULT NULL,
   `player_name` varchar(50) DEFAULT NULL,
   `cash` int(11) DEFAULT 0,
   `cash_account` int(11) DEFAULT 0,
   `inventory` longtext DEFAULT '{}',
+  `clothing` longtext DEFAULT '{}',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
-
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la table onset. account_administrator
-CREATE TABLE IF NOT EXISTS `account_administrator` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `account_id` int(11) NOT NULL,
-  `admin_level` int(11) NOT NULL,
-  `last_at_admin_state` timestamp NULL DEFAULT NULL,
-  `create_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
--- Les données exportées n'étaient pas sélectionnées.
-
--- Listage de la structure de la procédure onset. add_administrator
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `add_administrator`(
-	IN `a_id` INT,
-	IN `privilege_level` INT
-
-
-
-
-)
-BEGIN
-INSERT INTO onset.account_administrator (account_id, level, create_at) VALUES (a_id, privilege_level, NOW());
-END//
-DELIMITER ;
-
--- Listage de la structure de la table onset. ban
-CREATE TABLE IF NOT EXISTS `ban` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `reason` varchar(50) NOT NULL DEFAULT 'none',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
