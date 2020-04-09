672515367d6fa6bec23f6e0c9b8a9c9fef90b874
Jonathan <jonathan.gleyze@gmail.com>
2020-04-09 17:46:48 +0200

    add alert key info on spawn

--- init/server/init.lua
+++ init/server/init.lua
@@ -8,5 +8,9 @@ AddEvent("OnPlayerJoin", OnPlayerJoin)
 function OnPlayerSpawn(player)
 	local id = _Init_player.clothing_preset
 	CallRemoteEvent(player, "spawn_init_clothing", id)
+	Delay(9000, function()
+		SendAlert(player, "info", "Key", "press <strong>" .. _Key_ui.info:upper() .. "</strong> key to know the keys")
+	end)
 end
 AddEvent("OnPlayerSpawn", OnPlayerSpawn)
+
