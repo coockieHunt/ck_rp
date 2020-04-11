e90a244f98ffcc3050f088cfd97e14704a9c960a
Jonathan <jonathan.gleyze@gmail.com>
2020-04-11 16:59:42 +0200

    fix timer error if no player

--- account/server/account_manager.lua
+++ account/server/account_manager.lua
@@ -20,18 +20,21 @@ function IfCachedPlayer(player)
 end
 
 function OnPackageStart()
-    CreateTimer(function()
-        local countSaved = 0
-
-        for key, value in pairs(playerData) do
-            if(value.active) then
-                countSaved = countSaved + 1
-                SaveAccountPlayer(value.id_client)
+        CreateTimer(function()
+            if(tablelength(playerData) > 0) then
+                local countSaved = 0
+        
+                for key, value in pairs(playerData) do
+                    if(value.active) then
+                        countSaved = countSaved + 1
+                        SaveAccountPlayer(value.id_client)
+                    end
+                end
+                
+                if(countSaved > 0) then print("> Automatic backup account: "..countSaved.." backup") end
+                
             end
-        end
-
-        print("> Automatic backup account: "..countSaved.." backup")
-    end, _Account_timer.save_account_time)
+        end, _Account_timer.save_account_time)
 end
 AddEvent("OnPackageStart", OnPackageStart)
 
