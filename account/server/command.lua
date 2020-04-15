c95bbffc5dfaee8eb61db668a88e380893749536
Jonathan <jonathan.gleyze@gmail.com>
2020-04-15 16:39:04 +0200

    [add] time and help cmd

--- account/server/command.lua
+++ account/server/command.lua
@@ -1,4 +1,17 @@
 function cmd_saveAccount(playerid)
 	SaveAccountPlayer(playerid)
 end
+
+function cmd_time(playerid)
+	AddPlayerChat(playerid, getCurrentDayTime())
+end
+
+function cmd_help(playerid)
+	AddPlayerChat(playerid, "- /save (save your account)")
+	AddPlayerChat(playerid, "- /time (get current time)")
+end
+
+AddCommand("help", cmd_help)
+AddCommand("time", cmd_time)
 AddCommand("save", cmd_saveAccount)
+
