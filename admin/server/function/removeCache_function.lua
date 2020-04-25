32bf72c6bceb12bbcfbdb813e43fd4ffea9eb0d3
Jonathan <jonathan.gleyze@gmail.com>
2020-04-25 18:38:28 +0200

    [fix] save data before kick

--- admin/server/function/removeCache_function.lua
+++ admin/server/function/removeCache_function.lua
@@ -1,5 +1,6 @@
 function admin_remove_cache_player(player, id)
 	AddAdminLog(player, "admin :"..GetPlayerName(player).." remove cache for : " .. GetPlayerName(id))
+	SaveAccountPlayer(id)
 	local steam_id = GetPlayerSteamId(id)
 	KickPlayer(id,"😨 you were kicked by an administrator who works on your user data 😨" )
 	Delay(1000, function()
