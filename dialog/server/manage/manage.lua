07c0ac2c9f9f2a708436dd37bae477882a6dc668
Jonathan <jonathan.gleyze@gmail.com>
2020-04-30 07:21:35 +0200

    [add] OpenDialogClient/showMouse/freezePlayer

--- dialog/server/manage/manage.lua
+++ dialog/server/manage/manage.lua
@@ -1,6 +1,10 @@
-function CloseDialog(playerId, DialogId)
-    CallRemoteEvent(playerId, "closeDialog", DialogId)
+function CloseDialogClient(playerId, DialogId)
+    CallRemoteEvent(playerId, "CloseDialog", DialogId)
 end
+
+function OpenDialogClient(playerId, DialogId)
+    CallRemoteEvent(playerId, "OpenDialog", DialogId)
+ end
 
 function DestroyWebUI(playerId, DialogId)
     CallRemoteEvent(playerId, "DestroyWebUI", DialogId)
@@ -9,3 +13,12 @@ end
 function ExecWebJs(playerId, DialogId, cmd)
     CallRemoteEvent(playerId, "ExecWebJs", DialogId, cmd)
 end
+
+function ShowMouse(playerId, bool)
+    CallRemoteEvent(playerId, "ShowMouse", bool)
+end
+
+function FreezePlayerInput(playerId, bool)
+    CallRemoteEvent(playerId, "FreezePlayerInput", bool)
+end
+
