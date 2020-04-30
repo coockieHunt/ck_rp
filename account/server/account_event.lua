3128fc7f579a2a2093ec44062fb6d8df1fb5a336
Jonathan <jonathan.gleyze@gmail.com>
2020-04-30 18:24:55 +0200

    [fix] player say chat

--- account/server/account_event.lua
+++ account/server/account_event.lua
@@ -2,3 +2,7 @@ function OnPlayerDeath(player, instigator)
     OnplayerRespawn(player)
 end
 AddEvent("OnPlayerDeath", OnPlayerDeath)
+
+AddEvent("OnPlayerChat", function(playerId, text)
+	AddPlayerChatAll(_("on_player_chat", '"#43d815"', GetPlayerName(playerId), text))
+end)
