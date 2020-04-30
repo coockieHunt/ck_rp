42a66f4ebc4577a07af67408a472b78b61be40de
Jonathan <jonathan.gleyze@gmail.com>
2020-04-30 07:22:14 +0200

    [add] open/close client side

--- dialog/client/manage/cl_manage.lua
+++ dialog/client/manage/cl_manage.lua
@@ -1,12 +1,22 @@
-function closeDialog(webid)
+function CloseDialog(webid)
     for id, data in pairs(__dialogList) do
         if id == webid then
             SetWebVisibility(data.ui , WEB_HIDDEN)
-            CallRemoteEvent("OnCloseDialog", webid)
+            CallRemoteEvent("OnCloseDialog", id)
         end
     end
 end
-AddRemoteEvent("closeDialog", closeDialog)
+AddRemoteEvent("CloseDialog", CloseDialog)
+
+function OpenDialog(webid)
+    for id, data in pairs(__dialogList) do
+        if id == webid then
+            SetWebVisibility(data.ui , WEB_VISIBLE)
+            CallRemoteEvent("OnOpenDialog", id)
+        end
+    end
+end
+AddRemoteEvent("OpenDialog", OpenDialog)
 
 function DestroyWebUI(webid)
     for id, data in pairs(__dialogList) do
@@ -26,3 +36,22 @@ function ExecWebJs(webid, cmd)
     end
 end
 AddRemoteEvent("ExecWebJs", ExecWebJs)
+
+function ShowMouse(bool)
+    ShowMouseCursor(bool)
+
+    if(bool) then
+        SetInputMode(INPUT_GAMEANDUI)
+    else
+        SetInputMode(INPUT_GAME)
+    end
+end
+AddRemoteEvent("ShowMouse", ShowMouse)
+
+
+function FreezePlayerInput(bool)
+    SetIgnoreLookInput(bool)
+    SetIgnoreMoveInput(bool)
+end
+AddRemoteEvent("FreezePlayerInput", FreezePlayerInput)
+
