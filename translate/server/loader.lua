3b1a4d7b3ebc09f52da6ea8d8290b990bd247e3c
Jonathan <jonathan.gleyze@gmail.com>
2020-04-30 16:43:41 +0200

    [add] params on translate

--- translate/server/loader.lua
+++ translate/server/loader.lua
@@ -19,8 +19,6 @@ function OnPlayerJoin(playerId)
     local translate_list = json_decode(fileContent)
 
     OnTranslateStart(playerId)
-       
-
         translate = translate_list
         sendClient(playerId, translate_list)
     OnTranslateEnd(playerId)
@@ -28,19 +26,25 @@ end
 AddEvent("OnPlayerJoin", OnPlayerJoin)
 
 function sendClient(playerId, list)
-    -- print(list.hello_string_test)
     CallRemoteEvent(playerId, "GetTranslate", list)
 end
 
-function _(string)
-    return translate[string]
+function _(string, ...)
+    local text = translate[string]
+    
+    if isnil(text) then print('translate error ' .. string) end
+
+    local params = {...}
+    for i=1,#params do
+        text = text:gsub("{"..i.."}", params[i])
+    end
+
+    return text
 end
 
 function OnTranslateStart(playerId)
-    AddPlayerChat(playerId, "Loading translate please wait ...")
 end
 
 function OnTranslateEnd(playerId)
-    AddPlayerChat(playerId, "Loading translate end !!")
     translate_ready = true
 end
