0dd56fa65c28d62d08ad738bb542e6841081afa6
Jonathan <jonathan.gleyze@gmail.com>
2020-05-09 20:24:29 +0200

    [add] basique module car account

---
+++ admin/server/modules/create_car_account.lua
@@ -0,0 +1,38 @@
+local module = {
+    name = "create account",
+    id = "create_car_account",
+    select = "car",
+    level = 1
+}
+
+function module:GetName()
+    return module.name
+end
+
+function module:GetSelect()
+    return module.select
+end
+
+function module:GetId()
+    return module.id
+end
+
+function module:GetLevel()
+    return module.level
+end
+
+-- func
+function module:OnBuild()
+    AddForm('player', "target", "target", {})
+    AddForm('vehicles', "vehicles", "car_id", {})
+    AddForm('color', "color", "color", {})
+end
+
+function module:OnOpen(playerId)
+end
+
+function module:Onexecute(playerId, data)
+    RequestCreateCarAccount(data.target, data.car_id, data.color)
+end
+
+AddAdminModule(module)
---
+++ admin/server/modules/spawn_car_account.lua
@@ -0,0 +1,38 @@
+local module = {
+    name = "spawn account",
+    id = "spawn_car_account",
+    select = "car",
+    level = 1
+}
+
+function module:GetName()
+    return module.name
+end
+
+function module:GetSelect()
+    return module.select
+end
+
+function module:GetId()
+    return module.id
+end
+
+function module:GetLevel()
+    return module.level
+end
+
+-- func
+function module:OnBuild()
+    AddForm('player', "target", "target", {})
+    AddForm('text', 'car_id', 'car_id', {})
+
+end
+
+function module:OnOpen(playerId)
+end
+
+function module:Onexecute(playerId, data)
+    SpawnCarAccount(data.target, data.car_id)
+end
+
+AddAdminModule(module)
