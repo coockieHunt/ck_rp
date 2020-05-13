2b09ed1101bbfaba4fa976bd7ede65c29ec59bf8
Jonathan <jonathan.gleyze@gmail.com>
2020-05-13 15:02:18 +0200

    [fix] admon food/thirst out of range

--- admin/server/function/survival_function.lua
+++ admin/server/function/survival_function.lua
@@ -9,6 +9,10 @@ function admin_heal(player, target, num)
 	AddAdminLog(player, GetPlayerName(player).." set heal ".. GetPlayerName(target) .." | " .. num)
 	SetPlayerHealth(target, num)
 	local p = getplayer(target)
+
+	if num < 0 then num = 0 end
+	if num > 100 then num = 100 end
+
 	p:setHealth(num)
 
 	RefrechSurvivalIventoryUi(target)
@@ -20,6 +24,10 @@ function admin_heal(player, target, num)
 	AddAdminLog(player, GetPlayerName(player).." set armor ".. GetPlayerName(target) .." | " .. num)
 	SetPlayerArmor(target, num)
 	local p = getplayer(target)
+
+	if num < 0 then num = 0 end
+	if num > 100 then num = 100 end
+
 	p:setArmor(num)
 	
 	RefrechSurvivalIventoryUi(target)
@@ -30,6 +38,10 @@ function admin_heal(player, target, num)
 	AddAdminLog(player, GetPlayerName(player).." set thirst ".. GetPlayerName(target) .." | " .. num)
 
 	local p = getplayer(target)
+
+	if num < 0 then num = 0 end
+	if num > 100 then num = 100 end
+
 	p:setThirst(num)
 	
 	RefrechSurvivalIventoryUi(target)
@@ -39,6 +51,10 @@ function admin_heal(player, target, num)
  function admin_food(player, target, num)
 	AddAdminLog(player, GetPlayerName(player).." set food ".. GetPlayerName(target) .." | " .. num)
 	local p = getplayer(target)
+
+	if num < 0 then num = 0 end
+	if num > 100 then num = 100 end
+
 	p:setFood(num)
 	
 	RefrechSurvivalIventoryUi(target)
