cc5a3ae9f055103e222b411a52ccbc6d685e8c35
Jonathan <jonathan.gleyze@gmail.com>
2020-05-18 11:02:09 +0200

    [remove] type for item

--- files_client/web/script/window/survival.js
+++ files_client/web/script/window/survival.js
@@ -1,9 +1,3 @@
-$( function() {
-    AddItem("energi_drink", "test", 3, "../../files_client/web/src/img/energi_drink.png", "food" , 5 , 4 , 0,"Red Bull Energy Drink, souvent abrégée en Red Bull, est une boisson énergisante autrichienne créée par Red Bull GmbH ", "#ffdb4d", "thirst" )
-    AddItem("banana", "test", 3, "../../files_client/web/src/img/banana.png", "food" , 6 , 7 , 0, "a banane est le fruit ou la baie dérivant de l'inflorescence du bananier. Les bananes sont des fruits très généralement stériles issus de variétés domestiquées.", "#66c2ff", 'food')
-    AddItem("chips", "test", 15, "../../files_client/web/src/img/chips.png", "food" , 5 , -8, 0, "Les chips ou pommes chips, connues aussi sous le néologisme de croustilles au Canada, ", "#66c2ff", "food")
-    AddItem("firsy_aid_kit", "test", 15, "../../files_client/web/src/img/first_aid_kit.png", "medic" , 0 , 0, 5, "Les chips ou pommes chips, connues aussi sous le néologisme de croustilles au Canada, ", "#66c2ff", "food")
-});
 
 
 var dom_select = {
@@ -89,32 +83,26 @@ function clearInventory() {
     $('#items').empty(); 
 }
 
-function AddItem(id, name, quantity, thumb, type , thirst, food, health, descrip, color_type, icon_type){
+function AddItem(id, name, quantity, thumb, color, descrip ){
     let item = {
         "id" : id,
         "name" : name,
         "quantity" : quantity,
         "thumb" : thumb,
-        "type" : type,
-        "thirst" : thirst,
-        "food" : food,
-        "health" : health,
-        "descrip" : descrip,
-        "color_type" : color_type,
-        "icon_type" : icon_type,
+        "color" : color,
+        "descrip" : descrip
     }
 
     list_item.push(item);
-
 
     let object_div = "<div class='object' id="+ id +"></div>"
     let thumb_div = "<div class='thumb'></div>"
     let info_div = "<div class='info'></div>"
 
     let thumb_element = " <img src='" + thumb + "'>"
-    let type_element = "<div class='type'>" + BuildSvg(icon_type, "icon") + "</div>"
     let name_element = "<span class='name'>" + name + "</span>"
     let quantity_element = "<span class='quantity'>x " + quantity + "</span>"
+
 
     if($('#' + id).length == 0){
         $('#items').append(object_div); 
@@ -125,7 +113,6 @@ function AddItem(id, name, quantity, thumb, type , thirst, food, health, descrip
     
         if ( $('#' + id).text().length == 0 ) {
             $( '#' + id ).children('.thumb').append(thumb_element);  
-            $( '#' + id ).children('.info').append(type_element);  
             $( '#' + id ).children('.info').append(name_element);  
             $( '#' + id ).children('.info').append(quantity_element); 
         }
@@ -171,7 +158,7 @@ function SelectItem(id) {
         $(this).removeClass("qtihover")
     });
 
-    SetItemInfo(item['name'], item['thirst'], item['food'], item['health'], item['descrip'], item['color_type'])
+    SetItemInfo(item['name'], item['descrip'], item['color'])
 }
 
 $(dom_select.info_quantity_up).click(function() {
@@ -228,33 +215,15 @@ $(dom_select.info_quantity_down).click(function() {
     }
 });
 
-function SetItemInfo(name, thirst, food, healt, descrip, type_color)
+function SetItemInfo(name, descrip, color)
 {
     $(dom_select.info_name).text(name)
-    $(dom_select.info_name).css("color", type_color)
-    console.log(healt)
+    $(dom_select.info_name).css("color", color)
 
     $(dom_select.info_thirst).show()
     $(dom_select.info_food).show()
     $(dom_select.info_health).show()
 
-    if(food == 0) {
-        $(dom_select.info_food).hide()
-    }else{
-        $(dom_select.info_food + " > p").text(food)
-    }
-
-    if(thirst == 0) {
-        $(dom_select.info_thirst).hide()
-    }else{
-        $(dom_select.info_thirst + " > p").text(thirst)
-    }
-
-    if(healt == 0) {
-        $(dom_select.info_health).hide()
-    }else{
-        $(dom_select.info_health + " > p").text(healt)
-    }
     $(dom_select.info_description).text(descrip)
     $(dom_select.info_quantity).text(1)
 }
