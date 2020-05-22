f2b75d2719f4ffb0932d3320c2224fd4bcb28deb
Jonathan <jonathan.gleyze@gmail.com>
2020-05-22 17:19:56 +0200

    [add] sound loop, sync on disconect

--- sound/client/cl_sound_manager.lua
+++ sound/client/cl_sound_manager.lua
@@ -1,18 +1,33 @@
 local sound = {}
 
-function NewSoun3dSync(var, files, x, y, z, distance_sound)
+function NewSoun3dSync(var, files, x, y, z, distance_sound, renew)
     local newSound = CreateSound3D(files, x, y, z, distance_sound)
-    sound[var] = newSound
+    sound[var] = {
+        ['var'] = var,
+        ['files'] = files,
+        ['pos'] = {x , y, z},
+        ['distance'] = distance_sound, 
+        ['id'] = newSound,
+        ['renew'] = renew
+    }
 end
 AddRemoteEvent("NewSoun3dSync", NewSoun3dSync)
 
 function DeleteSoun3dSync(var)
-    DestroySound(sound[var])
+    DestroySound(sound[var].id)
+    sound[var] = nil
 end
 AddRemoteEvent("DeleteSoun3dSync", DeleteSoun3dSync)
 
 AddEvent("OnSoundFinished", function(sound_end)
-    if sounde == sound['car_alarm'] then
-        
+    for id, data in pairs(sound) do
+        if sound_end == data.id then
+            if data.renew then
+                local pos = data.pos
+                NewSoun3dSync(data.var, data.files, pos[1], pos[2], pos[3], data.distance, data.renew)
+            else
+                DeleteSoun3dSync(data.var)
+            end
+        end
     end
 end)
--- sound/server/sound_manager.lua
+++ sound/server/sound_manager.lua
@@ -1,18 +1,29 @@
-function NewSoun3d(var, files, x, y, z, distance)
+local sound = {}
+
+function NewSoun3d(var, files, x, y, z, distance, renew)
     local count_player = tablelength(GetAllPlayers())
     if count_player > 0 then
         for _, player in pairs(GetAllPlayers()) do
-            CallRemoteEvent(player,"NewSoun3dSync", var, files, x, y, z, distance)
+            sound[var] = {var, files, x, y, z, distance, renew}
+            CallRemoteEvent(player,"NewSoun3dSync", var, files, x, y, z, distance, renew)
         end
     end
 end
 
-function DeleteSoun3d(files, x, y, z, distance)
+function DeleteSoun3d(var)
     local count_player = tablelength(GetAllPlayers())
     if count_player > 0 then
         for _, player in pairs(GetAllPlayers()) do
+            sound[var] = nil
             CallRemoteEvent(player,"DeleteSoun3dSync", var)
         end
     end
 end
+
+function OnPlayerSpawn(playerid)
+    for id, data in pairs(sound) do
+        NewSoun3d(data[1], data[2], data[3], data[4], data[5], data[6], data[7])
+    end
+end
+AddEvent("OnPlayerSpawn", OnPlayerSpawn)
 
