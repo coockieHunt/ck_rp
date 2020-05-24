160a79e235a82aac6c9d455c92d907b213d714c8
Jonathan <jonathan.gleyze@gmail.com>
2020-05-24 20:03:47 +0200

    [fix] store admin car

--- car/server/private/store.lua
+++ car/server/private/store.lua
@@ -1,20 +1,26 @@
 function store_car(playerId, id)
     if IsValidVehicle(id) then
-        local query = mariadb_prepare(db, _RequestSql.StoreCar, 
+        if GetVehiclePropertyValue(id, 'owner') ~= '[]' then
+            local query = mariadb_prepare(db, _RequestSql.StoreCar, 
             GetVehiclePropertyValue(id, 'owner'), 
             GetVehicleHealth(id), 
             GetVehicleLicensePlate(id), 
             tohex(GetVehicleColor(id)), 
             GetVehiclePropertyValue(id, 'upgrade'), 
-            GetVehiclePropertyValue(id, 'db_id')
-        )
+                GetVehiclePropertyValue(id, 'db_id')
+            )
 
-        mariadb_async_query(db, query)
+            mariadb_async_query(db, query)
 
-        SetOnTheRoad(GetVehiclePropertyValue(id, 'db_id'), false)
+            SetOnTheRoad(GetVehiclePropertyValue(id, 'db_id'), false)
 
-        DestroyVehicle(id)
+            DestroyVehicle(id)
+            return true
 
+        else
+            return false
+        end
+        return false
     end
 end
 AddCommand("store", store_car)
