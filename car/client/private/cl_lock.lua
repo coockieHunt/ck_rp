ba5ba39b022e3af39131f428a10a615a95e05c66
Jonathan <jonathan.gleyze@gmail.com>
2020-05-25 16:24:37 +0200

    [up] separate car lock func

--- car/client/private/cl_lock.lua
+++ car/client/private/cl_lock.lua
@@ -1,6 +1,6 @@
 function OnKeyPress(key)
     if key == "L" then
-        CallRemoteEvent('lock_unlock_vehicle', nereast)
+        CallRemoteEvent('lock_unlock_vehicle_nearest', nereast)
     end
 end
 AddEvent("OnKeyPress", OnKeyPress)
--- car/server/private/lock.lua
+++ car/server/private/lock.lua
@@ -1,4 +1,4 @@
-function lock_unlock_vehicle(playerId)
+function lock_unlock_vehicle_nearest(playerId)
     local car_nearest = GetNearestVehicles(
         playerId, 
         _Lock_unlock.distance
@@ -13,33 +13,37 @@ function lock_unlock_vehicle(playerId)
             local p = getplayer(playerId)
     
             if has_value(owner, p.id) then
-                local locked = GetVehiclePropertyValue(selected_car, 'locked')
-                local name =  GetNameVehicleByVar(GetVehicleModelName(selected_car))
-    
-
-                if locked then
-                    PlayVehicleLockSquance(playerId, selected_car)
-                    SetVehiclePropertyValue(selected_car, 'locked', false, true)
-                    SendAlert(playerId, 'warning', _('vehicle') , _('car_unlock', name))
-                    
-                else
-                    PlayVehicleLockSquance(playerId, selected_car)
-                    SetVehiclePropertyValue(selected_car, 'locked', true, true)
-                    SendAlert(playerId, 'ok', _('vehicle') , _('car_lock', name))
-                end
-
-                if GetVehiclePropertyValue(selected_car, 'alarm') then
-                    StopAlarm(selected_car)
-                end
+                lock_unlock_vehicle(selected_car, playerId)
             end
         end
     else
         return false
     end
 end
-AddRemoteEvent('lock_unlock_vehicle', lock_unlock_vehicle)
+AddRemoteEvent('lock_unlock_vehicle_nearest', lock_unlock_vehicle_nearest)
 
-function PlayVehicleLockSquance(playerId, vehicle_id)
+function lock_unlock_vehicle(selected_car, playerId)
+    local locked = GetVehiclePropertyValue(selected_car, 'locked')
+    local name =  GetNameVehicleByVar(GetVehicleModelName(selected_car))
+
+
+    if locked then
+        PlayVehicleLockSquance(selected_car)
+        SetVehiclePropertyValue(selected_car, 'locked', false, true)
+        SendAlert(playerId, 'warning', _('vehicle') , _('car_unlock', name))
+        
+    else
+        PlayVehicleLockSquance(selected_car)
+        SetVehiclePropertyValue(selected_car, 'locked', true, true)
+        SendAlert(playerId, 'ok', _('vehicle') , _('car_lock', name))
+    end
+
+    if GetVehiclePropertyValue(selected_car, 'alarm') then
+        StopAlarm(selected_car)
+    end
+end
+
+function PlayVehicleLockSquance(vehicle_id)
     SetVehicleLightEnabled(vehicle_id, true)
     local x, y, z = GetVehicleLocation(vehicle_id)
     NewSoun3d('car_open', 'files_client/sound/car_lock.mp3', x, y, z,  _Lock_unlock.distance_sound, false)
