7bcd0623b90ca58bc1f88d30088d10ebcda2a1bc
Jonathan <jonathan.gleyze@gmail.com>
2020-06-05 17:32:34 +0200

    [add] fuel can item

---
+++ items/server/items/fuel_can.item.lua
@@ -0,0 +1,34 @@
+local item = {
+    name = "fuel can",
+    var = "fuel_can",
+    description = "items description",
+    model = 507,
+    weight = 1,
+    thumbnail= "fuel_can.png",
+    color = "#ffdb4d"
+}
+
+--[=====[ 
+  EVENT
+--]=====]
+
+function item:OnCreate(playerId, quantity)
+end
+
+function item:OnDestroy(playerId, quantity)
+end
+
+function item:OnUse(playerId, quantity)
+end
+
+function item:OnDrop(playerId, quantity)
+  drop_item(playerId, item.var, quantity)
+end
+
+function item:OnPlayerDeath(playerId, instigator)
+end
+
+function item:OnPlayerSpawn(playerId)
+end
+
+AddItem(item)
