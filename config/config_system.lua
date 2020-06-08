1bde767ba4732568a66930ee3c859ead9a793b16
Jonathan <jonathan.gleyze@gmail.com>
2020-06-08 18:08:23 +0200

    [add] hit_type register

--- config/config_system.lua
+++ config/config_system.lua
@@ -98,3 +98,14 @@ _Registe_SkeletalMesh_player = {
     pants = "Clothing4",
     shoes = "Clothing5"
 }
+
+_Registe_HitType = {
+    1 = "HIT_AIR",
+    2 = "HIT_PLAYER",
+    3 = "HIT_VEHICLE",
+    4 = "HIT_NPC",
+    5 = "HIT_OBJECT",
+    6 = "HIT_LANDSCAPE",
+    7 = "HIT_WATER",
+    8 = "HIT_DOOR",
+}
